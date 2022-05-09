b52c15f6da46ee01937032a5bc7390d06bf4e02f
Stefan Maier <inf18229@lehre.dhbw-stuttgart.de>
2022-05-09 09:13:26 +0200

    Added XML Parser

---
+++ src/XML_Parser.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+
+class XML_Parser:
+    def __init__(self):
+        pass
+
+    def convert_xml(self,file_path:str = None):
+        self.xml_data = ET.parse(file_path)
+        self.root_data = self.xml_data.getroot()
+    def get_system_data(self):
+        self.system_block_data = self.root_data.findall("./Model/System/Block")
+        self.system_line_data = self.root_data.findall("./Model/System/Line")
+
+
+if __name__ == '__main__':
+    print("XML_parser")
+    test = XML_Parser()
+    test.convert_xml("..\\test\\Test_XML.xml")
+
