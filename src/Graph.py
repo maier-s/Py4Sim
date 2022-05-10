cf6260d4d203359de6992b35094a540b0cd1b54c
StefanMaier <51311672+maier-s@users.noreply.github.com>
2022-05-10 23:05:54 +0200

    adjacency matrix can now expand by adding new vertex

--- src/Graph.py
+++ src/Graph.py
@@ -1,12 +1,12 @@
 class Vertex:
     def __init__(self,id:int):
-        self.type = "Test"
+        self.container = "Test"
         self.id = id
-    def getType(self):
-        print(self.type)
+    def getContainer(self):
+        print(self.container)
 
 class Graph:
-    def __init__(self,size):
+    def __init__(self,size:int = 0):
         self.vertex = []
         self.adj_matrix = []
 
@@ -24,20 +24,39 @@ class Graph:
     def add_vertex(self):
         new_Vertex = Vertex(len(self.vertex))
         self.vertex.append(new_Vertex)
+        self.__appendAdjMatrix__()
         return new_Vertex
     def print_matrix(self):
+        print("Adjacency Matrix")
         for row in self.adj_matrix:
             print(row)
 
 
+    def __appendAdjMatrix__(self):
+        # Adding new column
+        for row in self.adj_matrix:
+            row.append(row[-1])
+        # Adding new row
+        if self.size == 0:
+            self.adj_matrix.append([0])
+        else:
+            self.adj_matrix.append([0]*len(self.adj_matrix[0]))
+        self.size = self.size + 1
+
+
+
+
 if __name__ == '__main__':
-    test = Graph(5)
+    test = Graph()
     v1 = test.add_vertex()
+    print(test.size)
     v2 = test.add_vertex()
     v3 = test.add_vertex()
     v4 = test.add_vertex()
+    print(test.size)
     v5 = test.add_vertex()
     v6 = test.add_vertex()
+    print(test.size)
     test.add_edge(v1,v2)
     test.add_edge(v2, v4)
     test.add_edge(v5, v6)
@@ -45,4 +64,6 @@ if __name__ == '__main__':
     test.add_edge(v1, v4)
     test.add_edge(v4, v6)
     test.print_matrix()
-    print(v1.type)
+    test.__appendAdjMatrix__()
+    test.print_matrix()
+    print(v1.container)
